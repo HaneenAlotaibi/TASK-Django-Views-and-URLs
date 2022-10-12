73ce9b5cfacfe0a4d81a9cb395c84a36a1daf93e
HaneenAlotaibi <haneen.alotaibi@ccse.ku.edu.kw>
2022-10-12 19:31:15 +0300

    finish

--- products/views.py
+++ products/views.py
@@ -4,12 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 def get_home(request):
-    prodect=Product.objects.get(id=1)
-    return HttpResponse(f"<h1>Welcom {prodect.name}</h1>")
+    return HttpResponse("<h1>Welcome</h1>")
 
 def get_product(request,Prodect_id):
     prodect=Product.objects.get(id=Prodect_id)
-    string1=(f'name: {prodect.name}' 
-    f' price: {prodect.price} '
-    f'description: {prodect.description}')
-    return HttpResponse(string1)    
+    # string1=(f'name: {prodect.name}' 
+    # f' price: {prodect.price} '
+    # f'description: {prodect.description}')
+    return HttpResponse(f"<p> name: {prodect.name} </p><p>price: {prodect.price} </p> <p> description: {prodect.description}</p>")    
