b1dbf213fdf4ac12cdc2ff5dc3579068c242c7b0
HaneenAlotaibi <haneen.alotaibi@ccse.ku.edu.kw>
2022-10-12 19:12:51 +0300

    finish

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+import string
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+def get_home(request):
+    prodect=Product.objects.get(id=1)
+    return HttpResponse(f"<h1>Welcom {prodect.name}</h1>")
+
+def get_product(request,Prodect_id):
+    prodect=Product.objects.get(id=Prodect_id)
+    string1=(f'name: {prodect.name}' 
+    f' price: {prodect.price} '
+    f'description: {prodect.description}')
+    return HttpResponse(string1)    
--- productshop/urls.py
+++ productshop/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-
+from products.views import get_home,get_product
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('home/', get_home),
+    path('products/<int:Prodect_id>/',get_product),
 ]
